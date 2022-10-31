7c014625889305af8a90c68b79bb97566f90c733
xion <xiongxiao1012@outlook.com>
2022-10-31 11:08:07 +0800

    add catch

--- src/ruler/utils/draw.ts
+++ src/ruler/utils/draw.ts
@@ -99,13 +99,18 @@ export const draw2 = (ctx: CanvasRenderingContext2D, config: any = defaultConfig
 	}
 };
 export const createHDCanvas = (canvas, w, h) => {
-	const ratio = window.devicePixelRatio || 1;
-	canvas.width = w * ratio; // 实际渲染像素
-	canvas.height = h * ratio; // 实际渲染像素
-	canvas.style.width = `${w}px`; // 控制显示大小
-	canvas.style.height = `${h}px`; // 控制显示大小
-	const ctx = canvas.getContext('2d');
-	ctx.scale(ratio, ratio);
-	// canvas 绘制
-	return ctx;
+	try {
+		const ratio = window.devicePixelRatio || 1;
+		canvas.width = w * ratio; // 实际渲染像素
+		canvas.height = h * ratio; // 实际渲染像素
+		canvas.style.width = `${w}px`; // 控制显示大小
+		canvas.style.height = `${h}px`; // 控制显示大小
+		const ctx = canvas.getContext('2d');
+		ctx.scale(ratio, ratio);
+		// canvas 绘制
+		return ctx;
+	} catch (e) {
+		console.log('error createHDCanvas', canvas, w, h);
+		return false;
+	}
 };
